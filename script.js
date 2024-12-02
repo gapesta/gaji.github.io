8bf904dd2e930bdc5fd5409ab3b8b65ff1600599
anak_gapesta <anakgapesta@gmail.com>
2024-12-02 22:39:32 +0800

    memperbaiki angkanya

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const num3 = document.getElementById("num3");
 const num4 = document.getElementById("num4");
 const num5 = document.getElementById("num5");
 const num6 = document.getElementById("num6");
+const num7 = document.getElementById("num7");
 const calculateBtn = document.getElementById("calculate-btn");
 const totalDisplay = document.getElementById("total");
 // Tangkap elemen tombol reset
@@ -19,19 +20,39 @@ calculateBtn.addEventListener("click", () => {
     let val4 = parseFloat(num4.value) || 0;
     let val5 = parseFloat(num5.value) || 0;
     let val6 = parseFloat(num6.value) || 0;
+    let ptg = parseFloat(num7.value) || 0;
     
     //hitung gaji pulang jam 7
     let jam7 = val1 * 55;
     let hrbsa = 35.02 * val2;
     let jam7mgu = 34.96 * val3;
     let setenghri = 21.88 * val4;
+    let jam9 = 68.32 * val5;
+    let ponteng = 80 * ptg;
 
-
-    // Hitung total
-    const total = jam7 + jam7mgu + setenghri + hrbsa + 300 - val5 - val6;
+    if (ponteng == 80){
+            // Hitung total
+    const total = jam7 + jam7mgu + setenghri + hrbsa + 300 + jam9 - ponteng - val6;
 
     // Tampilkan total ke dalam elemen di web
-    totalDisplay.textContent = total;
+    totalDisplay.textContent = Number(total.toFixed(2));
+    } else if(ponteng == 160) {
+        const total = jam7 + jam7mgu + setenghri + hrbsa + 300 + jam9 - ponteng - val6;
+
+        // Tampilkan total ke dalam elemen di web
+        totalDisplay.textContent = Number(total.toFixed(2));
+    } else if(ponteng == 240) {
+        const total = jam7 + jam7mgu + setenghri + hrbsa + 300 + jam9 - ponteng - val6;
+
+        // Tampilkan total ke dalam elemen di web
+        totalDisplay.textContent = Number(total.toFixed(2));
+    } else { 
+        const total = jam7 + jam7mgu + setenghri + hrbsa + 300 + jam9 - val6;
+
+        // Tampilkan total ke dalam elemen di web
+        totalDisplay.textContent = Number(total.toFixed(2));
+    }
+
 });
 
 
@@ -45,6 +66,7 @@ resetBtn.addEventListener("click", () => {
     num4.value = "";
     num5.value = "";
     num6.value = "";
+    num7.value = "";
 
     // Atur ulang total ke 0
     totalDisplay.textContent = 0;
